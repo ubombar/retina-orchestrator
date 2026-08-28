f518722956d6acaec9ce7c71920ccf7b0a310b50
Ufuk BOMBAR <11573312+ubombar@users.noreply.github.com>
2026-08-28 20:35:17 +0200

    add orch script for campaign4

--- scripts/orch.sh
+++ scripts/orch.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
+if [[ -z "${RETINA_SECRET:-}" ]]; then
+	echo "Error: RETINA_SECRET is not declared." >&2
+	exit 1
+fi
+
 CAPTURE_DIR="./captures/$(date -u +%Y%m%d_%H%M%S)"
+
+# RR configuration
+RR_LEARNING_RATE=0.5
+RR_SAMPLING_WIDTH=0.0
+RR_MIN_ISSUANCE_PERIOD=1s
+RR_MAX_ISSUANCE_PERIOD=12h
+RR_STARTING_ISSUANCE_PERIOD=10s
+RR_DISABLE_RESPONSIBLE_PROBING=true
+RR_DISABLE_STALENESS=true
 
 ./retina-orchestrator \
 	--api-addr=":8080" \
@@ -23,14 +37,14 @@ CAPTURE_DIR="./captures/$(date -u +%Y%m%d_%H%M%S)"
 	--capturer-channel-size=200000 \
 	--capturer-flush-period=1s \
 	--rr-seed=42 \
-	--rr-learning-rate=0.5 \
-	--rr-sampling-width=0.0 \
+	--rr-learning-rate="${RR_LEARNING_RATE}" \
+	--rr-sampling-width="${RR_SAMPLING_WIDTH}" \
 	--rr-impact-threshold=1.0 \
 	--rr-fie-history-capacity=6 \
-	--rr-min-issuance-period=1s \
-	--rr-max-issuance-period=12h \
+	--rr-min-issuance-period="${RR_MIN_ISSUANCE_PERIOD}" \
+	--rr-max-issuance-period="${RR_MAX_ISSUANCE_PERIOD}" \
 	--rr-admission-rate=1000 \
-	--rr-starting-issuance-period=1s \
+	--rr-starting-issuance-period="${RR_STARTING_ISSUANCE_PERIOD}" \
 	--rr-status-interval=1m \
 	--rr-period-dump-interval=10m \
 	--rr-insert-channel-size=1024 \
@@ -42,8 +56,8 @@ CAPTURE_DIR="./captures/$(date -u +%Y%m%d_%H%M%S)"
 	--rr-max-update-drain-per-issuance=5 \
 	--rr-max-insert-drain-per-issuance=5 \
 	--rr-default-impact-delay=1s \
-	--rr-disable-responsible-probing=true \
-	--rr-disable-staleness=true \
+	--rr-disable-responsible-probing="${RR_DISABLE_RESPONSIBLE_PROBING}" \
+	--rr-disable-staleness="${RR_DISABLE_STALENESS}" \
 	--rr-disable-period-adjustment-events=true \
 	--rr-disable-pd-inserted-events=true \
 	--rr-disable-period-dump-events=true \
